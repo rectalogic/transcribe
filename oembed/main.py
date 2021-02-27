fa824ae47fce067d3cc136f322c644be112ff1f0
Andrew Wason <rectalogic@rectalogic.com>
2021-02-27 12:39:52 -0500

    flask

--- oembed/main.py
+++ oembed/main.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("oembed.json")
+    return jsonify(
+        version="1.0",
+        type="video",
+        provider_name="YouTube Transcript",
+        width=425,
+        height=644,
+        title="Cureatr oEmbed Test",
+        html='<iframe src="https://transcribe.rectalogic.com/" width="425" height="644"></iframe>',
+    )
 
 app.run(host='0.0.0.0', port=8080)
