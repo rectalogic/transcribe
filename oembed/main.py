1f4e97d4945e74b03bdacbdd2384601299ce9752
Andrew Wason <rectalogic@rectalogic.com>
2021-02-27 12:30:56 -0500

    fix repl.it

--- oembed/main.py
+++ oembed/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/oembed.json')
+@app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("oembed.json")
 
 app.run(host='0.0.0.0', port=8080)
